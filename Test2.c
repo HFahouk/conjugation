de2469f07d596974ae76d9e592ab56043f00e2c8
HFahouk <h.fahouk@gmail.com>
2023-05-16 17:31:44 +0000

    Verbes du troisième groupe triés selon leur conjugaison

--- Test2.c
+++ Test2.c
@@ -6,12 +6,38 @@
    
 int main() {
   int l, n = 0;  
-  char rep[3], ter[2], ver[3];
-  char tpar[30][30] = {"yer", "i", "érer", "èr", "eser", "ès", "éder", "èd", "éger", "èg", "cer", "ç", "eler", "ell", " èl", "eter", "ett", "èt", "écher", "èch", "è"};
+  char rep[3]; 
+  char ter[2];
+  char ver[3];
+  char tpar[30][30] = {"yer", "i", "érer", "èr", "eser", "ès", "éder", "èd", "éger", "èg", "cer", "ç", "eler", "ell", " èl", "eter", "ett", "èt"};
+ /* char ter11[] = "i";
+  char ter1[] = "èr";
+  char ter2[] = "ès";
+  char ter3[] = "èd";
+  char ter4[] = "èg";
+  char ter5[] = "ç";
+  char ter6[] = "ell";
+  char ter62[] = "èl"; 
+  char ter7[] = "ett";
+  char ter72[] = "èt"; 
+  char ver11[] = "yer";
+  char ver1[] = "érer";
+  char ver2[] = "eser";
+  char ver3[] = "éder";
+  char ver4[] = "éger";
+  char ver5[] = "cer";
+  char ver6[] = "eler";
+  char ver7[] = "eter";*/
   char tr[] = "é";
   char apr [] = "'";
 
-  char verbe[200], verbe2[200], vert[100], verp[100], verp2[100], verg[100], verb[100], vere[200];
+  char verbe[200];
+  char verbe2[200];
+  char vert[100];
+  char verp[100];
+  char verp2[100];  
+  char verg[100];
+  char verb[100];
    
   char sjt[20][20] = {"Je ", "Tu ", "Il/Elle ", "Nous ", "Vous ", "Ils/Elles ", "J'"};
   char prm[20][20] = {"me", "te", "se", "nous", "vous", "se"};
@@ -29,15 +55,12 @@ int main() {
   char pas2[50][50] = {"is", "is", "it", "îmes", "îtes", "irent"}; // Passé simple
   char fut2[50][50] = {"irai", "iras", "ira", "irons", "irez", "iront"}; // Futur simple 
 
-  // Terminaisons des verbes du deuxième groupe
-  char pre3[50][50] = {"s", "s", "t", "ons", "ez", "ent"};  // Présent de l'indicatif
-  char imp3[50][50] = {"ais", "ais", "ait", "ions", "iez", "aient"}; // Imparfait
-  char pas3[50][50] = {"is", "is", "it", "îmes", "îtes", "irent"}; // Passé simple
-  char fut3[50][50] = {"drai", "dras", "dra", "drons", "drez", "dront"}; // Futur simple 
-
+  
  do {    
    FILE *fbv = NULL, *fbv2 = NULL, *fbv3 = NULL;
-   char ligne[200], ligne2[200], ligne3[200];
+   char ligne[200];
+   char ligne2[200];
+   char ligne3[200];
 
    fbv = fopen("Fverbe.txt", "r");
    fbv2 = fopen("Fverbe2.txt", "r");
@@ -52,7 +75,6 @@ int main() {
     l = strlen(verbe); 
     strcpy(vert, verbe); // Récupère le verbe pour garder le radical infinitif 
     strcpy(verb, verbe); // Récupère le verbe sans subir de changement dans le code (pour les conditions)
-    strcpy(verp, verbe); // Récupère le verbe pour le traitement des verbes particuliers
     if (verb[2] == '-') {
      strcpy(verbe, verbe+3);
      strcpy(vert, vert+3);      
@@ -63,7 +85,7 @@ int main() {
     if (verb[0] == 'e' || verb[0] == 'a' || verb[0] == 'h' || verb[0] == tr[0]) {
      strcpy(sjt[0], sjt[6]);
     }
-    
+    verbe[l - 2] = '\0';
             
    if (fbv == NULL || fbv2 == NULL || fbv3 == NULL) {
      printf("Impossible d'ouvrir le fichier\n");
@@ -75,7 +97,7 @@ int main() {
        if (strstr(ligne, verb) != NULL) {
          if(strlen(verb) == (strlen(ligne) - 1)) {
            n = 1;
-           verbe[l - 2] = '\0';
+
            if (verb[2] == '-') {
              printf("\nLe verbe se %s est un verbe du premier groupe.\n", vert);
            }
@@ -84,10 +106,11 @@ int main() {
            }
            sleep(1);
              
+           strcpy(verp, verbe); // Récupère le verbe pour le traitement des verbes particuliers
            strcpy(verg, verbe); // Récupère le verbe pour le traitement des verbes particuliers en ger
            verg[l - 1] = '\0';
            
-           if ((numl>=1 && numl<=251) || (numl>=495 && numl<=854) || (verb[l - 4] == 'e' && verb[l -3] != 'x') || strcmp(verb, "lécher") == 0) {
+          if ((numl>=1 && numl<=251) || (numl>=495 && numl<=854)) {
                if (numl>=1 && numl<=114){
                  strcpy(ver, tpar[0]);
                  strcpy(ter, tpar[1]);
@@ -127,43 +150,31 @@ int main() {
                else if (numl>=782 && numl<=862) {
                  strcpy(ver, tpar[15]);
                  strcpy(ter, tpar[17]);
-               }    
-               else if (strcmp(verb, "lécher") == 0) {
-                 strcpy(ver, tpar[18]);
-                 strcpy(ter, tpar[19]);
-               }  
-               if (verb[l - 4] == 'e' || (numl>=116 && numl<=233)) {
-                strcpy(vere, vert+l-4);
-                verp[l - 4] = '\0';
-                strcat(verp, tpar[20]);
-                strcat(verp, vere+1);
-                verp[l - 1] = '\0';
-              }
-              if ((numl>=1 && numl<=251) || (numl>=495 && numl<=854) || (strcmp(verb, "lécher") == 0)) {
-               char *ptr = strstr(verb, ver);
-               if (ptr) {  
-                 strncpy(verp, verb, ptr - verb);
-                 verp[ptr - verb] = '\0';
-                 strcat(verp, ter);
-                 strcat(verp, ptr + strlen(ver));
-                }
-              }   
-           }
+               }
              
-           if (verb[2] == '-') {  
+              char *ptr = strstr(verb, ver);
+              if (ptr) {  
+              strncpy(verp, verb, ptr - verb);
+              verp[ptr - verb] = '\0';
+              strcat(verp, ter);
+              strcat(verp, ptr + strlen(ver));
+              }
+             }
+             
+             if (verb[2] == '-') {
              strcpy(verp, verp+3); 
              strcpy(verg, verg+3);
-           }
-           if (verb[1] == apr[0]) {
+             }
+             if (verb[1] == apr[0]) {
              strcpy(verp, verp+2);
              strcpy(verg, verg+2);
-           }
-           strcpy(verbe2, verbe);
-           strcpy(verp2, verp);  
+            }
+            strcpy(verbe2, verbe);
+            strcpy(verp2, verp);  
 
             //                            Conjugue les verbes particuliers au présent                                         //
             printf("\nAu présent de l'indicatif, ce verbe se conjugue: \n");
-            if ((numl>=1 && numl<=251) || (numl>=503 && numl<=518) || (numl>=679 && numl<=862) || (verb[l - 4] == 'e') || (strcmp(verb, "lécher") == 0)) { 
+            if ((numl>=1 && numl<=251) || (numl>=503 && numl<=518) || (numl>=679 && numl<=862) || (verb[l - 4] == 'e')) { 
               if (verb[2] == '-') { // Cas des verbes prénominaux en se
                   for (i=0; i<=5; i++) {
                    if (i == 3) {
@@ -342,7 +353,7 @@ int main() {
 
               printf("\nAu futur simple, ce verbe se conjugue: \n"); 
                //                                             Conjugue les verbes particuliers au futur simple                                      //
-               if ((numl>=1 && numl<=251) || (numl>=503 && numl<=518) || (numl>=679 && numl<=862) || (verb[l - 4] == 'e') || (strcmp(verb, "lécher") == 0)) { 
+               if ((numl>=1 && numl<=251) || (numl>=503 && numl<=518) || (numl>=679 && numl<=862)) { 
                if (verb[2] == '-') { // Cas des verbes prénominaux en se
                   for (i=0; i<=5; i++) {
                    if (i == 3) {
@@ -430,8 +441,7 @@ int main() {
      while (fgets(ligne2, 100, fbv2) != NULL) {
        if (strstr(ligne2, verb) != NULL) {
          if(strlen(verb) == (strlen(ligne2) - 1)) {
-           n = 1;          
-           verbe[l - 2] = '\0'; 
+           n = 1;           
            if (verb[2] == '-') {
              printf("\nLe verbe se %s est un verbe du deuxième groupe.\n", vert);
            }
@@ -530,11 +540,9 @@ int main() {
        }
         numl2++;
      }
-     while (fgets(ligne3, 200, fbv3) != NULL) {
+     while (fgets(ligne3, 100, fbv3) != NULL) {
        if (strstr(ligne3, verb) != NULL) {
-         printf("aaaa\n");
          if(strlen(verb) == (strlen(ligne3) - 1)) {
-          printf("okk\n");
            n = 1;
            if (verb[2] == '-') {
              printf("\nLe verbe se %s est un verbe du troisième groupe.\n", vert);
@@ -543,32 +551,6 @@ int main() {
              printf("\nLe verbe %s est un verbe du troisième groupe.\n", vert);
            }
            sleep(1);
-
-           verbe[l - 3] = '\0';
-           strcpy(verp, verbe);
-           strcpy(verbe2, verbe);
-
-           if (numl3 >= 1 && numl <= 92) {
-            strcat(verbe, "d");
-           }
-           if (numl3 >= 94 && numl3 <= 135) {
-            strcat(verp, "gn");
-           }
-           if (numl3 >= 137 && numl3 <= 140) {
-            strcat(verbe, "d");
-            strcat(verp, "s");
-           }
-           if (numl3 >= 141 && numl3 <= 143) {
-            strcat(verbe, "d");
-            verp[strlen(verp) - 1] = '\0'; 
-            strcat(verp, "l");
-           }
-           if (numl3 >= 144 && numl3 <= 149) {
-            verp[strlen(verp) - 1] = '\0'; 
-            strcat(verp, "lv");
-           }
-           
-           printf("%s\n%s\n", verbe, verp);
              
           
        
